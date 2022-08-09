33f007629f52349211d61892a8514d3af4627f53
Kirill Salykin <kirill.salykin@gmail.com>
2022-08-09 12:49:34 +0200

    Make it work

--- src/main/scala/dev/zio/quickstart/ExampleApi.scala
+++ src/main/scala/dev/zio/quickstart/ExampleApi.scala
@@ -6,7 +6,7 @@ import caliban.GraphQL
 import caliban.GraphQL.graphQL
 import caliban.RootResolver
 import caliban.schema.Annotations.{GQLDeprecated, GQLDescription}
-import caliban.schema.{GenericSchema, Schema}
+import caliban.schema.Schema
 import caliban.wrappers.ApolloTracing.apolloTracing
 import caliban.wrappers.Wrappers.*
 import zio.*
@@ -14,26 +14,28 @@ import zio.stream.ZStream
 
 import scala.language.postfixOps
 
-object ExampleApi extends GenericSchema[ExampleService] {
+object ExampleApi {
 
   case class Queries(
-                      @GQLDescription("Return all characters from a given origin")
-                      characters: CharactersArgs => URIO[ExampleService, List[Character]],
-                      @GQLDeprecated("Use `characters`")
-                      character: CharacterArgs => URIO[ExampleService, Option[Character]]
-                    )
+      characters: CharactersArgs => URIO[ExampleService, List[Character]],
+      character: CharacterArgs => URIO[ExampleService, Option[Character]]
+  )
 
-  case class Mutations(deleteCharacter: CharacterArgs => URIO[ExampleService, Boolean])
+  case class Mutations(
+      deleteCharacter: CharacterArgs => URIO[ExampleService, Boolean]
+  )
 
-  case class Subscriptions(characterDeleted: ZStream[ExampleService, Nothing, String])
+  case class Subscriptions(
+      characterDeleted: ZStream[ExampleService, Nothing, String]
+  )
 
   implicit val roleSchema: Schema[Any, Role] = Schema.gen
   implicit val characterSchema: Schema[Any, Character] = Schema.gen
   implicit val characterArgsSchema: Schema[Any, CharacterArgs] = Schema.gen
   implicit val charactersArgsSchema: Schema[Any, CharactersArgs] = Schema.gen
 
-  val api: GraphQL[ExampleService] =
-    graphQL(
+  val api =
+    graphQL[ExampleService, Queries, Unit, Unit](
       RootResolver(
         Queries(
           args => ExampleService.getCharacters(args.origin),
@@ -42,11 +44,13 @@ object ExampleApi extends GenericSchema[ExampleService] {
         Mutations(args => ExampleService.deleteCharacter(args.name)),
         Subscriptions(ExampleService.deletedEvents)
       )
-    ) @@
-      maxFields(200) @@ // query analyzer that limit query fields
-      maxDepth(30) @@ // query analyzer that limit query depth
-      timeout(3 seconds) @@ // wrapper that fails slow queries
-      printSlowQueries(500 millis) @@ // wrapper that logs slow queries
-      printErrors @@ // wrapper that logs errors
-      apolloTracing // wrapper for https://github.com/apollographql/apollo-tracing
+    )
 }
+
+//      @@
+//      maxFields(200) @@ // query analyzer that limit query fields
+//      maxDepth(30) @@ // query analyzer that limit query depth
+//      timeout(3 seconds) @@ // wrapper that fails slow queries
+//      printSlowQueries(500 millis) @@ // wrapper that logs slow queries
+//      printErrors @@ // wrapper that logs errors
+//      apolloTracing // wrapper for https://github.com/apollographql/apollo-tracing
